e98cf8aa67b0879a26ca2e35c61eda7db835bf3b
samuel <samzdevop@gmail.com>
2025-10-08 16:29:55 +0100

    updated db

---
+++ prisma/migrations/20251007135826_added_created_at_to_user_modal/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "FinancialTransaction_type_idx" ON "FinancialTransaction"("type");
+
+-- CreateIndex
+CREATE INDEX "FinancialTransaction_date_idx" ON "FinancialTransaction"("date");
